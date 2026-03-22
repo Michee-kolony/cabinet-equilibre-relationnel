fa8670cc37b2d17b1d43c2d340c4f6f0bc459934
Michée Kolony <micheekolony71@gmail.com>
2026-03-22 20:55:13 +0100

    mises à jour

--- src/app/admin/publier/publier.component.ts
+++ src/app/admin/publier/publier.component.ts
@@ -1,131 +1,162 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { interval, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-publier',
   templateUrl: './publier.component.html',
-  styleUrl: './publier.component.css'
+  styleUrls: ['./publier.component.css']
 })
-export class PublierComponent implements OnInit {
+export class PublierComponent implements OnInit, OnDestroy {
 
   url = "https://api-equilibre.cloud/actualites";
-  loading = true;
 
   actualites: any[] = [];
+  loading = true;
+
   showModal = false;
-  
-  titre = "";
-  soustitre = "";
-  description = "";
-  image = "";
-  facebook = "";
-  twitter = "";
-  tiktok = "";
+  isEdit = false;
+  selectedId: string = '';
+
+  // champs
+  titre = '';
+  soustitre = '';
+  description = '';
+  image = '';
+  facebook = '';
+  twitter = '';
+  tiktok = '';
+
+  // RXJS
+  refreshSub!: Subscription;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
+
+    // chargement initial
     this.getActualites();
+
+    // auto refresh toutes les 5 secondes
+    this.refreshSub = interval(5000)
+      .pipe(
+        switchMap(() => this.http.get<any[]>(this.url))
+      )
+      .subscribe(res => {
+
+        this.actualites = res.sort((a: any, b: any) => {
+          return new Date(b.date).getTime() - new Date(a.date).getTime();
+        });
+
+        this.loading = false;
+      });
   }
 
-  // 🔥 GET (tri du plus récent)
+  ngOnDestroy() {
+    if (this.refreshSub) {
+      this.refreshSub.unsubscribe();
+    }
+  }
+
   getActualites() {
-    this.http.get<any[]>(this.url).subscribe(data => {
-      this.actualites = data.sort((a, b) =>
-        new Date(b.date).getTime() - new Date(a.date).getTime()
-      
-      );
+    this.http.get<any[]>(this.url).subscribe(res => {
+
+      this.actualites = res.sort((a: any, b: any) => {
+        return new Date(b.date).getTime() - new Date(a.date).getTime();
+      });
+
       this.loading = false;
     });
   }
 
-deleteActualite(id: string) {
-  // Demande confirmation à l'utilisateur
-  if (!confirm("Êtes-vous sûr de vouloir supprimer cette actualité ?")) {
-    return;
-  }
-
-  // Appel DELETE vers l'API
-  this.http.delete(`${this.url}/${id}`).subscribe({
-    next: () => {
-      alert('Actualité supprimée avec succès');
-
-      // Rafraîchir la liste des actualités
-      this.getActualites();
-    },
-    error: (err) => {
-      console.error(err);
-      alert('Erreur lors de la suppression. Veuillez réessayer.');
-    }
-  });
-}
-
-
-publieractu() {
-  // Convertir les sauts de ligne en <br> pour l'affichage HTML
-  const descriptionFormattee = this.description.replace(/\n/g, '<br>');
-
-  const actu = {
-    titre: this.titre,
-    soustitre: this.soustitre,
-    description: descriptionFormattee, // on envoie la version formatée
-    image: this.image,
-    facebook: this.facebook,
-    twitter: this.twitter,
-    tiktok: this.tiktok
-  };
-
-  // Vérification des champs obligatoires
-  if (!this.titre || !this.soustitre || !this.description || !this.image) {
-    alert('Veuillez remplir tous les champs obligatoires !');
-    return;
-  }
-
-  // Envoi POST vers l'API
-  this.http.post('https://api-equilibre.cloud/actualites', actu).subscribe({
-    next: (res: any) => {
-      alert('Actualité publiée avec succès');
-
-      // Réinitialiser les champs
-      this.titre = '';
-      this.soustitre = '';
-      this.description = '';
-      this.image = '';
-      this.facebook = '';
-      this.twitter = '';
-      this.tiktok = '';
-
-      // Fermer le modal si tu utilises showModal
-      if (this.showModal) {
-        this.showModal = false;
-      }
-
-      // Rafraîchir la liste des actualités
-      this.getActualites();
-    },
-    error: (err) => {
-      console.error(err);
-      alert('Erreur lors de la publication. Veuillez réessayer.');
-    }
-  });
-}
-
-  
-
-  // 🔥 OPEN MODAL
+  // OUVRIR MODAL CREATION
   openModal() {
+    this.resetForm();
+    this.isEdit = false;
     this.showModal = true;
   }
 
-  // 🔥 CLOSE MODAL
+  // FERMER
   closeModal() {
     this.showModal = false;
   }
 
- 
+  // RESET FORM
+  resetForm() {
+    this.titre = '';
+    this.soustitre = '';
+    this.description = '';
+    this.image = '';
+    this.facebook = '';
+    this.twitter = '';
+    this.tiktok = '';
+  }
 
+  // CREER
+  publieractu() {
+    const data = {
+      titre: this.titre,
+      soustitre: this.soustitre,
+      description: this.description,
+      image: this.image,
+      facebook: this.facebook,
+      twitter: this.twitter,
+      tiktok: this.tiktok
+    };
 
-  // 🔥 FORMAT DATE SIMPLE
+    this.http.post(this.url, data).subscribe(() => {
+      this.getActualites();
+      this.closeModal();
+    });
+  }
+
+  // EDIT
+  editActualite(act: any) {
+    this.isEdit = true;
+    this.showModal = true;
+
+    this.selectedId = act._id;
+
+    this.titre = act.titre;
+    this.soustitre = act.soustitre;
+    this.description = act.description;
+    this.image = act.image;
+    this.facebook = act.facebook;
+    this.twitter = act.twitter;
+    this.tiktok = act.tiktok;
+  }
+
+  // UPDATE
+  updateActualite() {
+    const data = {
+      titre: this.titre,
+      soustitre: this.soustitre,
+      description: this.description,
+      image: this.image,
+      facebook: this.facebook,
+      twitter: this.twitter,
+      tiktok: this.tiktok
+    };
+
+    this.http.put(`${this.url}/${this.selectedId}`, data)
+      .subscribe(() => {
+        this.getActualites();
+        this.closeModal();
+      });
+  }
+
+  // DELETE
+  deleteActualite(id: string) {
+    if (confirm("Supprimer cette actualité ?")) {
+      this.http.delete(`${this.url}/${id}`)
+        .subscribe(() => {
+          this.getActualites();
+        });
+    }
+  }
+
+  // FORMAT DATE
   formatDate(date: string) {
     return new Date(date).toLocaleDateString();
   }
